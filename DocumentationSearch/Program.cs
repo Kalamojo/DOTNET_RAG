8bd76d23f445d0c01093b8239ca86401e4cf3200
Kalamojo <kojo22444488888888@gmail.com>
2025-01-29 21:39:37 -0600

    Embedding extraction POC

---
+++ DocumentationSearch/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.AI;
+using OpenAI;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var openAIApiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
+
+IEmbeddingGenerator<string, Embedding<float>> generator = new OpenAIClient(openAIApiKey)
+    .AsEmbeddingGenerator("text-embedding-3-small");
+
+Console.WriteLine(generator);
+
+var embedding = await generator.GenerateEmbeddingAsync("monopoly");
+
+Console.WriteLine(embedding);
